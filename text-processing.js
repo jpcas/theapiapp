33d656ff7db655dd2e117b74c0e2eaa2600ccd1a
JP Castrillon <jpcas001@gmail.com>
2021-02-04 15:18:27 -0500

    creating api request and processing api:

---
+++ text-processing.js
@@ -0,0 +1,9 @@
+// Text Proccesing API//
+const API_URL = 'https://japerk-text-processing.p.rapidapi.com/sentiment/';
+//RAPID API Request//
+const REQUEST_HEADERS = {
+    'X-RapidAPI-Host': 'japerk-text-processing.p.rapidapi.com'
+, 'X-RapidApi-Key':
+'7xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    'Content-Type': 'application/x-www-form-urlencoded'
+};
